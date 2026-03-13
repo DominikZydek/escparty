73626de8c1842550d8c36ea5178f425eb85ebb61
Dominik Żydek <dominik.zydek@edu.uekat.pl>
2026-03-13 08:44:29 +0100

    trying to not get blocked by last.fm

--- app/actions/image.ts
+++ app/actions/image.ts
@@ -9,11 +9,21 @@ async function fetchFromLastFm(artistQuery: string): Promise<string[]> {
 
     console.log(`[DEBUG] 🚀 Start pobierania dla: ${artistQuery} | URL: ${url}`);
 
+    // robust response to not get slapped a 406 code
     const response = await fetch(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Sec-Ch-Ua": '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1"
       },
-      // cache: "no-store" - polecam zostawić to na czas testów!
       cache: "no-store",
     });
 
